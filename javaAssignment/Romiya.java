3d7dabbaf6d6e523614839d0d466aa703c3f43ee
romiyadangol <dangolromiya@gmail.com>
2023-02-13 19:38:57 +0545

    initial commit

--- javaAssignment/Romiya.java
+++ javaAssignment/Romiya.java
@@ -1,21 +1,17 @@
 public class Romiya{
     public static void main(String[] args)
     {
-        System.out.println("");
-        System.out.println("");
-        System.out.println("Name of the organization: Canvas Corner");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("Address:");
+        
+        System.out.println("\nName of the organization: Canvas Corner");
+       
+        System.out.println("\nAddress:");
         System.out.println("----State: KM");
         System.out.println("----Zip Code: 131300");
         System.out.println("----Country: Nepal");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("Contact Information:");
+        System.out.println("\nContact Information:");
         System.out.println("----Mobile Number: 9745332324");
         System.out.println("----Email: canvas13@gmail.com");
-        System.out.println("");
-        System.out.println("");
+        System.out.println("\n");
+        
     }
 }
