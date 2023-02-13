ff528d2fb07a9a1b40d2e71b698de4e4526bc585
Romiya Dangol <91256132+romiyadangol@users.noreply.github.com>
2023-02-13 19:55:35 +0545

    initial commit

--- javaAssignment/Romiya.java
+++ javaAssignment/Romiya.java
@@ -1,7 +1,6 @@
 public class Romiya{
     public static void main(String[] args)
     {
-        
         System.out.println("\nName of the organization: Canvas Corner");
         System.out.println("\nAddress:");
         System.out.println("----State: KM");
@@ -10,7 +9,6 @@ public class Romiya{
         System.out.println("\nContact Information:");
         System.out.println("----Mobile Number: 9745332324");
         System.out.println("----Email: canvas13@gmail.com");
-        System.out.println("\n");
-        
+        System.out.println("\n"); 
     }
 }
