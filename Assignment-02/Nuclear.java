306b5589e96e2e9e524e256936edff976797b692
romiyadangol <dangolromiya@gmail.com>
2023-02-15 20:37:54 +0545

    initial commit

---
+++ Assignment-02/Nuclear.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Nuclear {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+    
+        System.out.println("What is the total number of employees?");
+        int emp = input.nextInt();
+
+        System.out.println("What is the name of the supervisor?");
+        String sname = input.next();
+
+        System.out.println("How many times the nuclear waste is removed from the core?");
+        int wasteRem = input.nextInt();
+
+        System.out.println("What is the total weight of waste produced every 1 week?");
+        float weeklyWasteWt = input.nextFloat();
+
+        System.out.println("Is the Electric Motor being replaced every 18 days? Press \"y\" for yes and \"n\" for no.");
+        char isReplaced = input.next().charAt(0);
+         if(isReplaced == 'y')
+        {
+            System.out.println("Yes");
+        }
+        else if(isReplaced == 'n')
+        {
+            System.out.println("No");
+        }
+        else
+        {
+            System.out.println("Invalid Character!!");
+        }
+
+        System.out.println("What is the core average temperature (Celsius) of the Nuclear Reactor?");
+        double coreTemp = input.nextFloat();
+        double coreTempKelvin = coreTemp + 273.15;
+        System.out.println("\n\n");
+        System.out.println("Update on Nuclear Reactor Status for JX00-XC-HB1 located in the East of Japan:");
+        System.out.println("Total number of employees: " + emp);
+        System.out.println("Supervisor name: " + sname);
+        System.out.println("Nuclear waste was removed from the core : " + wasteRem + "times");
+        System.out.println("Total weight of nuclear waste produced every 1 week: " + weeklyWasteWt);
+        System.out.println("Is the Electric Motor being replaced every 18 days? " + isReplaced);
+        System.out.println("Core average temperature in Celsius: " + coreTemp + "°C");
+        System.out.println("Core average temperature in Kelvin: " + coreTempKelvin + "K");
+        System.out.println("\n\n");
+    }
+}
