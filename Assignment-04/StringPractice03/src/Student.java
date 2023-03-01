c935597e88bf8515eeee007466796d53d304bf52
Romiya Dangol <91256132+romiyadangol@users.noreply.github.com>
2023-03-01 20:04:49 +0545

    initial commit

--- Assignment-04/StringPractice03/src/Student.java
+++ Assignment-04/StringPractice03/src/Student.java
@@ -1,10 +1,12 @@
 public class Student {
     String name;
     int roll_no;
+    //constructor
     Student(String n,int r){
         name = n;
         roll_no = r;
     }
+    //function
     void Student(){
         System.out.println("Name : "+name);
         System.out.println("Roll_no : "+roll_no);
