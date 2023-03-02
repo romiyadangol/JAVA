fd814f092eb847ae3969f194ddeccedfb397a191
romiya dangol <dangolromiya@gmail.com>
2023-03-02 19:12:31 +0545

    initial commit

--- ClassObject/Student/src/Student.java
+++ ClassObject/Student/src/Student.java
@@ -1,25 +1,11 @@
 public class Student {
-<<<<<<< HEAD:ClassObject/StringPractice03/src/Student.java
     String name ;
     int roll_no ;
-
-
-   // constructor
-=======
-    String name;
-    int roll_no;
-    //constructor
->>>>>>> c935597e88bf8515eeee007466796d53d304bf52:Assignment-04/StringPractice03/src/Student.java
-    Student(String n,int r){
+    Student(String n,int r) {
         name = n;
         roll_no = r;
     }
-<<<<<<< HEAD:ClassObject/StringPractice03/src/Student.java
 
-   // function
-=======
-    //function
->>>>>>> c935597e88bf8515eeee007466796d53d304bf52:Assignment-04/StringPractice03/src/Student.java
     void Student(){
         System.out.println("Name : "+name);
         System.out.println("Roll_no : "+roll_no);
