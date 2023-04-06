001ac4be7bd83c2a90f5b2481d888e592bee3664
Romiya Dangol <91256132+romiyadangol@users.noreply.github.com>
2023-04-06 18:33:58 +0545

    Update Bird.java

--- Assignment-04/src/Correctcode/Bird.java
+++ Assignment-04/src/Correctcode/Bird.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package Correctcode;
 
 public class Bird extends Animal {
@@ -8,15 +8,4 @@ public class Bird extends Animal {
     public void walk(){
         System.out.println("I am walking");
     }
-=======
-package Correctcode;
-
-public class Bird extends Animal {
-    public void fly() {
-        System.out.println("I am Flying");
-    }
-    public void walk(){
-        System.out.println("I am walking");
-    }
->>>>>>> 91755e13d01a5612251b6b83e29c097e6ac9dd72
 }
