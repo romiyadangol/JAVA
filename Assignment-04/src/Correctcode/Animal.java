796e366107b5a79a94ad78ee3c3c78961d113466
Romiya Dangol <91256132+romiyadangol@users.noreply.github.com>
2023-04-06 18:33:35 +0545

    Update Animal.java

--- Assignment-04/src/Correctcode/Animal.java
+++ Assignment-04/src/Correctcode/Animal.java
@@ -1,18 +1,7 @@
-<<<<<<< HEAD
 package Correctcode;
 
 public class Animal {
     public void eat() {
         System.out.println("I am Eating");
     }
-
-=======
-package Correctcode;
-
-public class Animal {
-    public void eat() {
-        System.out.println("I am Eating");
-    }
-
->>>>>>> 91755e13d01a5612251b6b83e29c097e6ac9dd72
 }
